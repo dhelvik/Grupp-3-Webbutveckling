3baab96581510eb9938f4218a52fdd0274a740f7
dhelvik <daniel.helvik.93@gmail.com>
2018-11-28 16:42:37 +0100

    Update lastUpdated.js

--- js/lastUpdated.js
+++ js/lastUpdated.js
@@ -2,6 +2,10 @@ $(document).ready(lastUpdated);
 function lastUpdated(){
     var x = document.getElementById("lastUpdated");
     var days = x.getDate;
-    x.innerHTML = "Senast uppdaterad: " + document.lastModified;
+    var dateTime = new Date(document.lastModified);
+    var date = dateTime.getDate();
+    var year = dateTime.getFullYear();
+    var month = dateTime.getMonth();
+    x.innerHTML = "Senast uppdaterad: " + date + "/" + month + "-" + year;
 
 }
