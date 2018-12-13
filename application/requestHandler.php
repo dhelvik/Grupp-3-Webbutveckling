d3ab07aa258d687fd1c225e6d4f9011cb082e0f3
Jacob <jacob.hollviken@telia.com>
2018-12-13 12:49:37 +0100

    Update application/requestHandler.php

--- application/requestHandler.php
+++ application/requestHandler.php
@@ -1,10 +1,16 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    
-}
+include 'controller.php';
+include 'model.php';
+session_start();
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     
 }
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    switch ($_POST['ACTION']){
+        case '':
+    }
+}
+
 
 
 $method = $_SERVER['REQUEST_METHOD'];
