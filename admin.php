170752b6bd9dae6c712453f9bd635e64891b6a7c
dhelvik <daniel.helvik.93@gmail.com>
2018-12-14 14:30:48 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -10,13 +10,39 @@
     <div id="main">
 		<div id="login">
 			<h2>Admin Login</h2>
-			<form action="" method="post">
+			<form id="loginForm" action="" method="post">
 				<label>Användarnamn :</label> 
 				<input id="name" name="username" placeholder="username" type="text">
 				<label>Lösenord :</label>
 				<input id="password" name="password" placeholder="**********" type="password"> 
 				<input name="submit" type="submit" value=" Login ">
+				<input name="ACTION" value="checkLogin" type="hidden">
 			</form>
+			<script>
+			$(function(){
+				$("#loginForm").submit(function(e){
+				e.preventDefault();
+			$.ajax({
+				method: "POST",
+				url: "application/requestHandler.php", 
+				data: $("#loginForm").serialize(),
+				datatype: 'json',
+				
+				success: function(result){
+					
+					
+						});
+					
+				},
+				error: function(xhr, status, error){
+					alert(status);
+					//$('#labelEntryResponse').text('');
+					//$('#labelEntryResponse').text(status);
+				}
+			});
+		});
+	
+	</script>
 		</div>
 	</div>
     <?php
