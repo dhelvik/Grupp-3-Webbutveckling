c19b151a646ab2a0c6705f5e0fd498cc51305894
Jacob <jacob.hollviken@telia.com>
2019-01-06 13:27:44 +0100

    edits

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost", "root", "root", "webbutveckling");
-$sql = "DELETE FROM Karnevalist WHERE mail = '".$_POST["mail"]."'";
-if(mysqli_query($connect, $sql))
-{
-    echo $POST["mail"]. 'Data Deleted';
-}
-?>
